493239eb86575c5e6aef219255d5cb86fea9e45d
Epistoteles <korbinian-koch@web.de>
2021-11-11 17:00:02 -0500

    initial commit

---
+++ UTKFaceDataset.py
@@ -0,0 +1,84 @@
+
+import torch
+from torch.utils.data import Dataset
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+from torchvision import transforms
+import random
+
+torch.set_printoptions(linewidth=120)
+
+# from google.colab import drive, files
+# drive.mount('/content/gdrive')
+# root_dir = 'gdrive/My Drive/Colab Notebooks/Unlearning Fairness/'
+# %cd 'gdrive/My Drive/Colab Notebooks/Unlearning Fairness/'
+
+class UTKFace(Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, image_dir='UTKFace'):
+        self.image_dir = image_dir
+        self.filenames = [f for f in listdir(image_dir) if 
+                      isfile(join(image_dir, f)) and 
+                      len(f.split('_')) == 4 and  # wrong names
+                      f not in {'1_0_0_20170109193052283.jpg.chip.jpg',
+                                '1_0_0_20170109194120301.jpg.chip.jpg'} and  # damaged 👀
+                      (f.split('_')[0] != 26 or bool(random.getrandbits(1)))  # throw away 50% of 26-year-olds (too many)
+        ]
+        self.images = []
+        self.ages = list(map(lambda x: int(x.split('_')[0]), self.filenames))
+        self.genders = list(map(lambda x: int(x.split('_')[1]), self.filenames))
+        self.races = list(map(lambda x: int(x.split('_')[2]), self.filenames))
+
+        for f in self.filenames:
+          image_path = join(self.image_dir, f)
+          temp = Image.open(image_path)
+          keep = temp.copy()
+          self.images.append(keep)
+          temp.close()
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.filenames)
+
+  def __getitem__(self, index, label='age'):
+        'Generates one sample of data'
+        image = self.images[index]
+
+        if label == 'age':
+          age_bins = [5, 19, 24, 27, 30, 35, 40, 50, 61, 120]  # up to x years
+          age = self.ages[index]
+          for idx, bin in enumerate(age_bins):
+            if age <= bin:
+              label = idx
+              break
+          if label is 'age':
+            raise ValueError(f'Unknown age encountered: {age}')
+        elif label == 'gender':
+          label = self.gender[index]
+        elif label == 'race':
+          label = self.race[index]
+        else:
+          raise ValueError('label type not known')
+
+        transform = transforms.Compose([
+          transforms.RandomHorizontalFlip(),
+          transforms.ColorJitter(brightness=0.1, contrast=0.2, saturation=0.1, hue=0.0),
+          transforms.RandomAffine(degrees=15, translate=None, scale=(0.9, 1.2), shear=10, fill=0),
+          transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.)),
+          transforms.RandomAutocontrast(),
+          transforms.Resize((227,227)),
+          transforms.ToTensor(),
+          transforms.Normalize(mean=[152.01048, 116.37661, 99.60926], std=[32.09281, 29.17887, 30.77170])
+        ])
+
+        return (transform(image), label)
+
+  @staticmethod
+  def denormalize(image):
+        'Undoes the normalization transform for viewing and plotting'
+        denorm = transforms.Compose([
+          transforms.Normalize(mean = [0., 0., 0.], std=[1/32.09281, 1/29.17887, 1/30.77170]),
+          transforms.Normalize(mean = [-152.01048, -116.37661, -99.60926], std = [1., 1., 1.])
+        ])
+        return denorm(image)
