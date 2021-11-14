9d04ee3b41ca5aa57c1c2ca255ccbef815c014d2
Epistoteles <korbinian-koch@web.de>
2021-11-14 15:28:26 -0500

    add checkpointing

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning import Trainer
 from AgeModelCNN import AgeModelCNN
 from AgeModelVGG import AgeModelVGG
@@ -10,7 +10,12 @@ from AgeModelResnet18 import AgeModelResnet18
 model = AgeModelResnet18()
 
 logger = WandbLogger(project="age-classifier", entity='epistoteles')
-lr_monitor = LearningRateMonitor(logging_interval='epoch')
-trainer = Trainer(max_epochs=15, gpus=1, logger=logger, callbacks=[lr_monitor])
+lr_monitor_cb = LearningRateMonitor(logging_interval='epoch')
+checkpoint_cb = ModelCheckpoint(monitor="val/macro_f1_epoch",
+                                dirpath="checkpoints/run1/",
+                                filename="agemodel-shard=1-{epoch:02d}-{val/macro_f1_epoch:.2f}",
+                                save_top_k=1,
+                                mode='max')
+trainer = Trainer(max_epochs=15, gpus=1, logger=logger, callbacks=[lr_monitor_cb, checkpoint_cb])
 
 trainer.fit(model)
