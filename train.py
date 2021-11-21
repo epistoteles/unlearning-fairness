42c5d3cd7ce662dd972f4f665ee42fc5b99f6bdb
Epistoteles <korbinian-koch@web.de>
2021-11-21 18:26:24 -0500

    group run and reduce epochs

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ import itertools
 import wandb
 from utils import random_run_name
 
-
 run_name = random_run_name()
 print(f"Starting experiment run {run_name} ...")
 
@@ -27,16 +26,18 @@ for current_shard, current_slice in itertools.product(range(num_shards), range(n
             current_slice=current_slice,
             num_slices=num_slices)
 
-
-    logger = WandbLogger(project="age-classifier", entity='epistoteles', id=f'{run_name}-shard-{current_shard}-slice-{current_slice}')
+    logger = WandbLogger(project="age-classifier",
+                         entity='epistoteles',
+                         id=f'{run_name}-shard-{current_shard}-slice-{current_slice}',
+                         group=f'{run_name}')
     lr_monitor_cb = LearningRateMonitor(logging_interval='epoch')
-    checkpoint_cb = ModelCheckpoint(monitor=None,  # saves checkpoint for last epoch
+    checkpoint_cb = ModelCheckpoint(save_top_k=1,
+                                    # monitor=None,  # saves checkpoint for last epoch
+                                    monitor='val/macro_f1_epoch',  # saves checkpoint for best epoch
                                     dirpath=f"checkpoints/{run_name}/",
                                     filename=f"{run_name}-shard={current_shard}-slice={current_slice}",
-                                    save_top_k=1,
                                     save_weights_only=True)
-    trainer = Trainer(max_epochs=15, gpus=1, logger=logger, callbacks=[lr_monitor_cb, checkpoint_cb])
+    trainer = Trainer(max_epochs=13, gpus=1, logger=logger, callbacks=[lr_monitor_cb, checkpoint_cb])
 
     trainer.fit(model)
     wandb.finish()
-
